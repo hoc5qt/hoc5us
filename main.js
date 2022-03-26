00a6b3881c27d9d01126ca9f5ce15b10b7898997
Frank.He <frank.he@datavisor.com>
2022-03-26 07:58:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-document.getElementById("iframeTag").src = "https://hoc5.us/ch/";
+// document.getElementById("iframeTag").src = "https://hoc5.us/ch/";
+document.getElementById("iframeTag").src = "https://www.google.com/";
